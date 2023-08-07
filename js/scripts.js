0fdb38009b99b52c21da268cbb1f5bfa0b060cbf
Alejandro De La Cruz <alejantony.22@hotmail.com>
2023-08-07 10:54:24 -0500

    aeaaaaaaaaaa

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-
+const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 const slider = document.getElementById('slider');
 
 const setTheme = (theme) => {
@@ -6,6 +6,9 @@ const setTheme = (theme) => {
     localStorage.setItem('theme', theme);
 }
 
+slider.addEventListener('click', ()  => {
+    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+    setTheme(switchToTheme);
+});
 
 setTheme(localStorage.getItem('theme') || preferedColorScheme);
-localStorage.clear;
